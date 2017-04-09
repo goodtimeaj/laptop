6f94e1e7f694de65a66de023795d4aec2bb3ef46
gregstallings <gregstallings@gmail.com>
2017-04-08 22:22:29 -0700

    Update ievms settings

--- setup/shared/ievms.sh
+++ setup/shared/ievms.sh
@@ -5,4 +5,4 @@
 # WARNING: Installation may take hours
 # Install size ~86G
 
-sudo curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | env REUSE_XP="no" IEVMS_VERSIONS="7 8 9 10 11" bash
+sudo curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | env REUSE_XP="no" IEVMS_VERSIONS="9 10 11 EDGE" bash
